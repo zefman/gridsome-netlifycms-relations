a81d861199e1fa15a23c0352e40011d855e12d1a
Jozef Maxted <jozef.maxted@stratumfive.com>
2019-06-26 13:14:09 +0100

    Add NetlifyCMS collection types and ID widget

--- gridsome.config.js
+++ gridsome.config.js
@@ -33,7 +33,8 @@ module.exports = {
     {
       use: `gridsome-plugin-netlify-cms`,
       options: {
-        publicPath: `/admin`
+        publicPath: `/admin`,
+        modulePath: `src/admin/index.js` // This is important and allows us to register our own widget
       }
     },
   ]
